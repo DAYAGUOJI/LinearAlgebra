90bb3d9ac082765db4d98e918ac046fea262d25e
Garterbelt <2651325241@qq.com>
2025-05-19 19:17:53 +0800

    first_save

---
+++ PlayLA/vector.py
@@ -0,0 +1,44 @@
+class Vector:
+
+    def __init__(self, lst):
+
+        """
+        Initialize the vector with a list of values.
+            
+        Args:
+        lst: List of numerical values to initialize the vector.
+        """
+        self._values = lst
+
+    def __add__(self, another):
+        """Adds another vector to this vector element-wise.
+            
+        Args:
+                another (Vector): The vector to add to this vector.
+                
+        Returns:
+                Vector: A new vector resulting from the element-wise addition.
+                
+        Raises:
+                AssertionError: If the vectors have different lengths.
+        """
+        assert len(self) == len(another), \
+            "Error in adding. Lengths of vectors must be the same."
+        return Vector([x + y for x, y in zip(self, another)])
+    
+    def __iter__(self):
+        return self._values.__iter__()
+
+    def __getitem__(self, index):
+        return self._values[index]
+
+    def __len__(self):
+        return len(self._values)
+
+    def __repr__(self):
+        """Returns the string representation of the vector in the format 'Vector(values)'."""
+        return 'Vector({})'.format(self._values)
+    
+    def __str__(self):
+        return '({})'.format(', '.join(str(x) for x in self._values))
+          
---
+++ main.vector.py
@@ -0,0 +1,7 @@
+from PlayLA.vector import Vector
+
+if __name__ == '__main__':
+    vec = Vector([1, 3])
+    print(vec)
+    print(len(vec))
+    print('vec[0] = {}, vec[1] = {}'.format(vec[0], vec[1]))
